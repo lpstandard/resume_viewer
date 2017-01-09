01fa967411e27a6bed9cefa17c87b0b1da8dc0f4
Tony Kung <lps.tony@gmail.com>
2017-01-08 20:01:13 -0600

    edit url for namecheap

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url: 'https://kung-resume.herokuapp.com/resumeData.json',
+      url: 'http://www.tonykung.tech/resumeData.json',
       dataType: 'json',
       cache: false,
       success: function(data) {
